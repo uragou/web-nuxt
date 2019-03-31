93d9d47099141604d6ac3abb91fba5a6866ca239
uragou <uragouryotaro@gmail.com>
2019-03-31 16:50:14 +0900

    ハンバーガーメニューのアニメーション作成

--- src/Head.vue
+++ src/Head.vue
@@ -1,10 +1,16 @@
 <template>
-  <div id="head" v-bind:class= "{actmenu:hum}">
-      <img id="humberger" @click = "hum = !hum" src="./assets/humberger.png">
-      <ul v-show = "hum" >
-        <router-link class = "menu" name = "home" tag="li" to="/"><a>home</a></router-link>
-        <router-link class = "menu" name = "contact" tag="li" to="/contact"><a>contact</a></router-link>
-      </ul>
+  <div id="head">
+    <transition name= "amenu" mode="out-in">
+      <div id="acthead" v-if= "hum" key= "act">
+        <div id="humberger"  @click = "hum = !hum">ー<br>ー<br>ー</div>
+        <ul>
+          <router-link class = "menu" name = "home" tag="li" to="/"><a>home</a></router-link>
+          <router-link class = "menu" name = "contact" tag="li" to="/contact"><a>contact</a></router-link>
+        </ul>
+      </div>
+      <div id="humberger"  @click = "hum = !hum" v-else key = "hid">ー<br>ー<br>ー</div>
+    </transition>
+    
   </div>
 </template>
 
@@ -21,17 +27,23 @@ export default {
 
 <style>
 #head{
-    position: fixed;
-    font-size: 20px;
-    font-weight:700;
-    color: black;
-    margin: 5;
-    padding: 0;
-    z-index: 9999;
+  position: fixed;
+  text-align: center;
+  font-size: 20px;
+  font-weight:700;
+  margin: 5;
+  padding: 0;
+  z-index: 9999;
 }
 
-.Hclass{
-  background-color: rgba(240, 248, 255, 0.7);
+#acthead{
+  position: fixed;
+  background-color: blueviolet;
+  width: 15%;
+  height: 100%;
+  margin: 5;
+  padding: 0;
+  z-index: 9999;
 }
 
 ul{
@@ -45,16 +57,29 @@ ul{
   padding: 20px;
 }
 
-.actmenu{
-  background-color: blueviolet;
-  width: 20%;
-  height: 100%;
-
-}
-
 #humberger{
-  margin: 10px;
+  cursor: default;
+  margin: 10px 30px;
+  line-height: 15px;
+  font-size: 60px;
 }
 
 
+.amenu-enter-active{
+  transition-property: all;
+  transition-duration: 0.6s;
+  transition-timing-function: linear;
+}
+.amenu-leave-active{
+  transition: opacity .5s;
+}
+
+.amenu-enter{
+  transform: translateX(-200px);
+  opacity: 0;
+}
+
+.amenu-leave-to{
+  opacity: 0;
+}
 </style>
